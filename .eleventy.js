dc4c20f05838be67ad80731d90cbbfa3152e79a0
Chaotic <pranjalprani11@gmail.com>
2026-03-24 09:34:52 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -525,6 +525,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPassthroughCopy("src/site/user");
+  eleventyConfig.addPassthroughCopy("src/site/canvas-viewer.html");
+  eleventyConfig.addPassthroughCopy("src/site/canvas");
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
